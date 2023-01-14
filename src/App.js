102fbf06b13cdb3808534ea69af27f5e5e84a819
markalink <102572099+markalink@users.noreply.github.com>
2023-01-13 23:01:44 -0500

    removed routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom"
 import Home from "./pages/home/Home"
-import CrazyDot from "./pages/crazydot/CrazyDot"
 
 const App = () => {
   return (
-    <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/crazydot" element={<CrazyDot />} />
-          {/* <Route path="*" element={<h1>404</h1>} /> */}
-        </Routes>
-    </Router>
+    <Home />
   )
 }
 
